96c2bfaf58a754112819e67dfbae431bba52cf6a
mcputra <drachansta@gmail.com>
2025-04-27 20:06:27 +0700

    add answer for unicafe

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h1>statistics</h1>
+      <table>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={good + neutral + bad} />
+        <StatisticLine
+          text="average"
+          value={(good * 1 - bad * 1) / (good + neutral + bad)}
+        />
+        <StatisticLine
+          text="positive"
+          value={((good * 1) / (good + neutral + bad)) * 100}
+        />
+      </table>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood((prevGood) => prevGood + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral((prevNeutral) => prevNeutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad((prevBad) => prevBad + 1);
+  };
+
+  return (
+    <>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text="good"></Button>
+      <Button handleClick={handleNeutralClick} text="neutral"></Button>
+      <Button handleClick={handleBadClick} text="bad"></Button>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  );
+};
+
+export default App;
