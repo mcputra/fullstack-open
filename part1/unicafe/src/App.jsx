ff35f363b458398c778c558c900cdaa1985419b0
mcputra <drachansta@gmail.com>
2025-07-06 16:30:00 +0700

    part1 - 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,17 +4,21 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
 
   const handleGood = () => {
     setGood(good + 1);
+    setAll(all + 1);
   };
 
   const handleNeutral = () => {
     setNeutral(neutral + 1);
+    setAll(all + 1);
   };
 
   const handleBad = () => {
     setBad(bad + 1);
+    setAll(all + 1);
   };
 
   return (
@@ -27,6 +31,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good * 1 + neutral * 0 + bad * -1) / all}</p>
+      <p>positive {(good / all) * 100}%</p>
     </>
   );
 };
