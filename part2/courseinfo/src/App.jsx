b7ae9225ca552bbce1b988533d09b2c68da4c799
mcputra <drachansta@gmail.com>
2025-07-06 20:11:28 +0700

    part2 - 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,40 +1,4 @@
-const Header = ({ name }) => {
-  return <h2>{name}</h2>;
-};
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ parts }) => {
-  return (
-    <>
-      {parts.map((part) => {
-        return <Part key={part.id} part={part} />;
-      })}
-    </>
-  );
-};
-
-const Total = ({ parts }) => {
-  const total = parts.reduce((acc, part) => acc + part.exercises, 0);
-
-  return <h3>total of {total} exercises</h3>;
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
