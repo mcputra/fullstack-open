ebc55c910eabddac0fa8898cac0fd4846326eb1f
mcputra <drachansta@gmail.com>
2025-07-14 21:14:02 +0700

    part2 - 2.19

--- part2/restcountries/src/App.jsx
+++ part2/restcountries/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Country from "./components/Country";
 import countriesService from "./services/countries";
 
 const App = () => {
@@ -20,28 +21,26 @@ const App = () => {
     );
   };
 
+  const handleShow = (name) => {
+    setFilteredCountries(
+      countries.filter((country) => country.name.common === name)
+    );
+  };
+
   let content = "";
 
   if (filteredCountries.length === 1) {
     content = filteredCountries.map((country) => (
-      <div key={country.name.common}>
-        <h1>{country.name.common}</h1>
-        <p>Capital: {country.capital}</p>
-        <p>Area: {country.area} km²</p>
-        <h2>Languages</h2>
-        <ul>
-          {Object.values(country.languages).map((language) => (
-            <li key={language}>{language}</li>
-          ))}
-        </ul>
-        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} />
-      </div>
+      <Country country={country} key={country.name.common} />
     ));
   } else if (filteredCountries.length > 10) {
     content = <p>Too many matches, specify another filter</p>;
   } else {
     content = filteredCountries.map((country) => (
-      <p key={country.name.common}>{country.name.common}</p>
+      <p key={country.name.common}>
+        {country.name.common}
+        <button onClick={() => handleShow(country.name.common)}>Show</button>
+      </p>
     ));
   }
 
