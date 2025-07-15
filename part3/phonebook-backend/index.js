73a124bdc6c63cc63e0b9baaa0b8b2db3f66448e
mcputra <drachansta@gmail.com>
2025-07-15 22:53:40 +0700

    part3 - 3.5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -60,6 +60,30 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
+const generateId = () => {
+  return Math.floor(Math.random() * 1_000_000).toString();
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name and number are required",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
