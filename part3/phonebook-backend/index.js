0565f78c8d6353aaebe6d8e59d1cc1ae519fb6e6
mcputra <drachansta@gmail.com>
2025-07-23 22:08:22 +0700

    part3 - 3.13

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,48 +1,31 @@
+require("dotenv").config();
 const express = require("express");
+const Person = require("./models/person");
+
 const app = express();
-const morgan = require("morgan");
 
-morgan.token("jsonData", function getJson(req) {
-  return JSON.stringify(req.body);
-});
+let persons = [];
 
-app.use(express.json());
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
 app.use(express.static("dist"));
-app.use(
-  morgan(
-    ":method :url :status :res[content-length] - :response-time ms :jsonData"
-  )
-);
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello Person!</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/info", (request, response) => {
@@ -54,14 +37,9 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -103,7 +81,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
