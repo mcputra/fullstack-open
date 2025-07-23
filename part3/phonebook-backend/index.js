ab26f3a7cc5fa755d9742d3e205044bfe7c539d5
mcputra <drachansta@gmail.com>
2025-07-23 18:51:22 +0700

    part3 - 3.9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 morgan.token("jsonData", function getJson(req) {
   return JSON.stringify(req.body);
 });
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static("dist"));
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :jsonData"
@@ -102,7 +105,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
